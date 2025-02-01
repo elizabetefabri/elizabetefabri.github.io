a7b8822ebc55ddede2c864f4dba6520088bba44b
Elizabete Fabri <elizabete958@gmail.com>
2025-02-01 19:48:53 -0300

    Create componentes iniciais do portifolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { TestimonialsComponent } from './pages/extras/testimonials/testimonials.component';
+import { CardComponent } from './shared/components/card/card.component';
+import { CoreModule } from './core/core.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent,
+    ResumeComponent,
+    TestimonialsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
