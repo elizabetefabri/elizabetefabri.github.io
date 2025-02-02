e285a524fbe59e3d91e89d640dee9f0b18e7c0d0
Elizabete Fabri <elizabete958@gmail.com>
2025-02-01 23:10:43 -0300

    add tipo de social media

---
+++ src/app/shared/models/social-media.model.spec.ts
@@ -0,0 +1,7 @@
+import { SocialMedia } from './social-media.model';
+
+describe('SocialMedia', () => {
+  it('should create an instance', () => {
+    expect(new SocialMedia()).toBeTruthy();
+  });
+});
---
+++ src/app/shared/models/social-media.model.ts
@@ -0,0 +1,7 @@
+// social-media.model.ts
+export interface SocialMediaLink {
+  image: string;
+  link: string;
+  alt: string;
+  title: string;
+}
