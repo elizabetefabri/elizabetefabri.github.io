332803f3b551259db3d4c2dc5b7f6f7d9256ef31
Elizabete Fabri <elizabete958@gmail.com>
2025-02-01 23:10:01 -0300

    Create: social media, feito as estilização e codigo otimizado

---
+++ src/app/core/components/social-media/social-media.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import type { SocialMediaLink } from 'src/app/shared/models/social-media.model';
+
+@Component({
+  selector: 'app-social-media',
+  templateUrl: './social-media.component.html',
+  styleUrls: ['./social-media.component.css']
+})
+export class SocialMediaComponent {
+  @Input() contatoTitle = "";
+  @Input() links: SocialMediaLink[] = [];
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,12 +5,14 @@ import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { DivComponent } from './components/div/div.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent,
-    DivComponent
+    DivComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
