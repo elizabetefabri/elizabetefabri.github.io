9ced6c3f2b3c48165091a92c2e2bb52717bda1a6
Elizabete Fabri <elizabete958@gmail.com>
2025-02-01 22:34:03 -0300

    add componentes ao modulo

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-
-
+import { RouterModule } from '@angular/router';
+import { DivComponent } from './components/div/div.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DivComponent
   ],
   imports: [
-    CommonModule
-  ]
+    BrowserModule,
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class CoreModule { }
