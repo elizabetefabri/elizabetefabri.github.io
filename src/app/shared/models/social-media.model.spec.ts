5fc48860d825b38b515f93743fcd53f2c45c75ea
Elizabete Fabri <elizabete958@gmail.com>
2025-06-19 18:17:33 -0300

    Auto-generated commit

--- src/app/shared/models/social-media.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SocialMedia } from './social-media.model';
-
-describe('SocialMedia', () => {
-  it('should create an instance', () => {
-    expect(new SocialMedia()).toBeTruthy();
-  });
-});
--- src/app/shared/models/social-media.model.ts
+++
@@ -1,7 +0,0 @@
-// social-media.model.ts
-export interface SocialMediaLink {
-  image: string;
-  link: string;
-  alt: string;
-  title: string;
-}
